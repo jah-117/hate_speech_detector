a9af3fd50a4cfe56e812ba23fb3aaa820024498e
jah-117 <jahfarbinmuhammed117@gmail.com>
2025-05-05 20:56:55 +0530

    version update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hate_speech_detector",
-    version="0.1.2", 
+    version="0.2.1", 
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
